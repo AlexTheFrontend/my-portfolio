aeafaa3a917eb177f92f92fbd88e505e02940638
Alexander <fedlogic@gmail.com>
2022-03-20 19:44:17 +1300

    A bit more to NavCss

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
@@ -9,30 +10,59 @@ export default function NavBar() {
           <NavLink
             to="/"
             exact
-            activeClassName="text-white"
-            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 transition-colors text-4xl font-bold cursive tracking-widest"
+            className="inline-flex items-center py-6 px-3 mr-4 text-white hover:text-sky-600 transition-colors text-4xl font-bold cursive tracking-widest"
           >
             Alexander
           </NavLink>
           <NavLink
             to="/post"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-pink-200 transition-colors hover:text-black"
           >
             Blog Post
           </NavLink>
           <NavLink
             to="/project"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-pink-200 transition-colors hover:text-black"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-pink-200 transition-colors hover:text-black"
           >
             About Me!
           </NavLink>
         </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://www.facebook.com/AleksFedorOFF"
+            className="mr-4"
+            target={"_blank"}
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://github.com/AlexTheFrontend"
+            className="mr-4"
+            target={"_blank"}
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/xanderfedorov/"
+            className="mr-4"
+            target={"_blank"}
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://twitter.com/AlexTheFrontend"
+            className="mr-4"
+            target={"_blank"}
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
       </div>
     </header>
   );
