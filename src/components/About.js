e3d1ed7dd7bd1d15d61c920687c8be6f9f4b5e49
Alexander <fedlogic@gmail.com>
2022-05-01 14:08:14 +1200

    Fixing img size

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
-import bg2 from "../images/bg2.jpg";
-import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
+import image from "../images/bg3.jpeg";
+import ImageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
 const builder = new ImageUrlBuilder(sanityClient);
@@ -30,28 +30,34 @@ export default function About() {
   }
 
   return (
-    <main className="relative">
-      <img src={bg2} alt="Auckland city" className="absolute w-full" />
-      <div className="p-10 lg:pt-48 container mx-auto relative"></div>
-      <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-        <img
-          src={urlFor(author.authorImage).url()}
-          className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
-          alt={author.name}
-        />
-        <div className="text-lg flex-col justify-center">
-          <h1 className="cursive text-6xl text-green-300 mb-4">
-            Hey there. I'm <span className="text-green-100">{author.name}</span>
-          </h1>
-          <div className="prose lg:prose-xl text-white">
-            <BlockContent
-              blocks={author.bio}
-              projprojectId="iacy8q5t"
-              dataset="production"
-            />
+    <main>
+      <img
+        src={image}
+        alt="Auckland city"
+        className="absolute object-cover w-full h-full"
+      />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-900 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            alt={author.name}
+          />
+          <div className="text-lg flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hey there. I'm{" "}
+              <span className="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projprojectId="iacy8q5t"
+                dataset="production"
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
